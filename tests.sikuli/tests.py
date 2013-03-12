1be2423f3799910f3a8bf6a2ed66c857715904aa
Janet <jed@pculture>
2013-03-12 20:31:28 +0100

    few changes to work on osx

--- tests.sikuli/tests.py
+++ tests.sikuli/tests.py
@@ -2,13 +2,21 @@ import os
 import sys
 from sikuli.Sikuli import *
 
-sys.path.append(os.path.join('/usr', 'local', 'lib', 'python2.7', 'dist-packages'))
-sys.path.append(os.path.join('/usr', 'local', 'lib', 'python2.7', 'site-packages'))
-if 'WINDOWS' in str(Env.getOS()):
+testos = str(Env.getOS())
+
+if 'LINUX' in testos:
+    sys.path.append(os.path.join('/usr', 'local', 'lib', 'python2.7', 'dist-packages'))
+    sys.path.append(os.path.join('/usr', 'local', 'lib', 'python2.7', 'site-packages'))
+elif 'WINDOWS' in testos:
     pkg_path = os.path.join(os.getcwd(), '..', 'env-miro', 'Lib', 'site-packages')
+    sys.path.append(pkg_path)
+elif 'MAC' in testos:
+    pkg_path = os.path.join(os.getenv('VIRTUAL_ENV'), 'lib', 'python2.7', 'site-packages')
     print pkg_path
     sys.path.append(pkg_path)
-sys.path.append(os.getcwd())
+else:
+    print 'no idea the os'
+
 import nose 
 
 nose.main()
